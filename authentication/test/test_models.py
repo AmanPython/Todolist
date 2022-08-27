f98111cabb246c6e8cfbcef76183049d6824d417
Aman_Gora <aman.gora@orchids.edu.in>
2022-08-27 11:44:39 +0530

    when something fails

---
+++ authentication/test/test_models.py
@@ -0,0 +1,17 @@
+from rest_framework.test import APITestCase
+from authentication.models import User 
+
+class TestModel(APITestCase):
+    
+    def test_create_user(self):
+        user = User.objects.create_user('cryce','crycetruly@gmail.com','password123!@')
+        self.assertIsInstance(user, User)
+        self.assertFalse(user.is_staff)
+        self.assertEqual(user.email, 'crycetruly@gmail.com')
+
+    def test_create_super_user(self):
+        user = User.objects.create_user('cryce','crycetruly@gmail.com','password123!@')
+        self.assertIsInstance(user, User)
+        self.assertTrue(user.is_staff)
+        self.assertEqual(user.email, 'crycetruly@gmail.com')
+
