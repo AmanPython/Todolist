7349a651ee5e40edffe30857fe4d180b271752ff
Aman_Gora <aman.gora@orchids.edu.in>
2022-08-27 12:30:19 +0530

    super user test

--- authentication/test/test_models.py
+++ authentication/test/test_models.py
@@ -32,9 +32,15 @@ class TestModel(APITestCase):
         with self.assertRaisesMessage(ValueError,'The given email must be set'):
             User.objects.create_user(username='crycetruly@gmail.com',email='', password = 'password@1')
         # with.self.assertRaisesMessage(ValueError,)
-        self.assertRaises(ValueError,User.objects.create_user,username="notset",email = "",password = 'password123!@')
+        # self.assertRaises(ValueError,User.objects.create_user,username="notset",email = "",password = 'password123!@')
 
+    def test_creates_superuser_with_super_user_status(self):
+        with self.assertRaisesMessage(ValueError,'Superuser must have is_staff=True.'):
+            User.objects.create_superuser(username='crycetruly@gmail.com',email='sdfds@gmail.com', password = 'password@1',is_staff=False)
     
+    def test_creates_superuser_with_super_user_status(self):
+        with self.assertRaisesMessage(ValueError,'Superuser must have is_superuser=True.'):
+            User.objects.create_superuser(username='crycetruly@gmail.com',email='sdfds@gmail.com', password = 'password@1',is_superuser=False)
 
         # user = User.objects.create_user(username = 'cryce',)
         # self.assertIsInstance(user, User)
